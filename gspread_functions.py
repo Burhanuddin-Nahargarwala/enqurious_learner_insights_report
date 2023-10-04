2a7af276a3654d34f6ba82f31c3fc8638e31eea7
Burhan523 <burhanuddin@mentorskool.com>
2023-10-04 20:05:39 +0530

    Modified main.js: Added the function update_assessment_data_with_google_sheet_data

--- gspread_functions.py
+++ gspread_functions.py
@@ -57,6 +57,16 @@ drive_service = build("drive", "v3", credentials=credentials)
 
 
 def search_folder(folder_name, parent_folder_id=None):
+    """Finds folders with a specific name on Google Drive.
+
+    Args:
+        folder_name (str): The name of the folder to search for on Google Drive.
+        parent_folder_id (str|None, optional): The ID of the parent folder to search within.
+            If None, the search will be performed in the root directory. Defaults to None.
+
+    Returns:
+        folders: A list of folders matching the specified criteria.
+    """
     if parent_folder_id:
         # Search for the folder by name
         query = f"'{parent_folder_id}' in parents and mimeType='application/vnd.google-apps.folder' and name='{folder_name}'"
@@ -78,9 +88,18 @@ def search_folder(folder_name, parent_folder_id=None):
     return folders
 
 
-def create_folder(
-    folder_name, parent_folder_id
-):  # parent_folder_id="1AzkjLoxMHu6oA7CQHSNeNBQTUKadF3_D"):
+def create_folder(folder_name, parent_folder_id=None):
+    """Creates a folder with the specified name on Google Drive.
+
+    Args:
+        folder_name (str): The name of the folder to be created on Google Drive.
+        parent_folder_id (str|None, optional): The ID of the parent folder in which the new folder
+            will be created. If None, the folder will be created in the root directory. Defaults to None.
+
+    Returns:
+        folder_id: The folder ID of the newly created folder.
+    """
+
     # Create the folder metadata
     folder_metadata = {
         "name": folder_name,
@@ -105,6 +124,21 @@ def create_folder(
 
 
 def conditional_formatting(google_sheet, sheet_name):
+    """Creates conditional formatting rules in a specific sheet of a Google spreadsheet.
+
+    Conditional formatting rules vary based on the sheet type:
+
+    - Masterclass - Progress sheet:
+        - Yellow color for values less than 100.
+
+    - Assessment - Progress sheet:
+        - Red color for values less than 70.
+
+    Args:
+        google_sheet (str): The name of the Google spreadsheet.
+        sheet_name (str): The name of the specific sheet within the spreadsheet.
+    """
+
     # fetch the sheet
     sheet = google_sheet.worksheet(sheet_name)
 
@@ -149,6 +183,14 @@ def conditional_formatting(google_sheet, sheet_name):
 
 
 def bold_text(sheet, cell_range, enable_bold: bool = True):
+    """Applies bold formatting to a specified cell range in a sheet.
+
+    Args:
+        sheet (str): The name of the sheet in which text should be formatted as bold.
+        cell_range (str): The cell range to which bold formatting should be applied.
+        enable_bold (bool, optional): If True, the text in the specified cell range will be bold.
+            If False, the bold formatting will be removed. Defaults to True.
+    """
     format = {"textFormat": {"bold": enable_bold}}
 
     sheet.format(cell_range, format)
@@ -156,17 +198,22 @@ def bold_text(sheet, cell_range, enable_bold: bool = True):
 
 def generate_progress_report_from_view(view_df, sheet_name, folder_id, file_name):
     """
-    prerequisites: If the function doesn't store the given df into the Google sheet then check that you
-    have given the editor access of your Google sheet to the given email id:- `generated-report@generated-report.iam.gserviceaccount.com`
-    Steps:-
-    1. Go to Google Sheet -> File -> Share
-    2. Click on Share with Others
-    3. Add the given email_id and give the editor access to it.
+    Prerequisites:
+    Ensure that the provided Google Sheet has been shared with the email address: `generated-report@generated-report.iam.gserviceaccount.com`.
 
-    :param client_name: name of the client whose learner's data is there in the view_df dataframe
-    :param view_df:  containing the learner's data of a particular client
+    To Share the Google Sheet:
+    1. Open the Google Sheet.
+    2. Click on "File" -> "Share."
+    3. Under "Share with others," add the email address and grant it editor access.
 
-    process: Store the view_df dataframe into the Google sheet.
+    Args:
+        view_df (DataFrame): The DataFrame containing learner data for a specific client.
+        sheet_name (str): The name of the sheet where the data will be stored.
+        folder_id (str): The ID of the folder in which the Google Sheet should be placed.
+        file_name (str): The desired name for the Google Sheet file.
+
+    Process:
+    Stores the data from the view_df DataFrame into a specified Google Sheet.
     """
     # console.cloud.google.com:- ```generated-report@generated-report.iam.gserviceaccount.com```
     scopes = [
@@ -262,8 +309,80 @@ def generate_progress_report_from_view(view_df, sheet_name, folder_id, file_name
     conditional_formatting(google_sheet=sh, sheet_name=sheet_name)
 
 
+def update_assessment_data_with_google_sheet_data(new_assessment_data_df, file_name, folder_id, sheet_name):
+    """
+    Updates the 'new_assessment_data_df' DataFrame with data from a specified Google Sheet. The attribute
+    presented in google sheet, but are not present in new_assessment_data_df, will be inserted in new_assessment_data_df
+    so that if any attribute reflected in google sheet won't be removed from the sheet.
+    
+    Parameters:
+    - new_assessment_data_df (pd.DataFrame): The DataFrame to be updated.
+    - file_name (str): The name of the Google Sheet file.
+    - folder_id (str): The folder ID where the Google Sheet is located.
+    - sheet_name (str): The name of the specific sheet within the Google Sheet.
+
+    Returns:
+    - pd.DataFrame: The updated DataFrame.
+    """
+    # Sort the records based on the participant_email
+    # new_assessment_data_df.sort_values("participant_email", inplace=True)
+
+    # Now fetch the data from Google Sheet
+    sheet_data_df = fetch_data_from_google_sheet(
+        file_name=file_name,
+        folder_id=folder_id,
+        sheet_name=sheet_name,
+    )
+
+    # Remove the column "Average"
+    if "Average" in sheet_data_df.columns:
+        sheet_data_df = sheet_data_df.drop(columns=["Average"])
+
+    # Also sort the records of sheet_data_df
+    # sheet_data_df.sort_values("participant_email", inplace=True)
+
+    # Find columns present in sheet_data_df but not in new_assessment_data_df
+    missing_columns = [
+        col for col in sheet_data_df.columns if col not in new_assessment_data_df.columns
+    ]
+
+    # if there are no any column that is present in the google sheet but are not there in the new_assessment_data_df
+    if not missing_columns:
+        print("There is no such column in google sheet that is not present in new_assessment_data_df!")
+        return new_assessment_data_df
+
+    # Insert the missing columns into new_assessment_data_df at the appropriate positions (after the last matching column)
+    #for col in missing_columns:
+    # print(f"{col} column is not present in new assessment sheet, but is there in the google sheet.")
+
+    # Now append participant_email with mssing columns for merge purpose
+    missing_columns.append("participant_email")
+
+    new_assessment_data_df = new_assessment_data_df.merge(
+            sheet_data_df[missing_columns],
+            how="inner",
+            on="participant_email"
+        )
+    
+    return new_assessment_data_df
+
+
 # Function to generate reports
 def generate_report(df, sheet_name, value_column, folder_id, file_name):
+    """Pivots the DataFrame, converting project names from rows to columns.
+    If the sheet name is 'Assessment - Progress,' it calculates the average of assessments.
+    The pivoted DataFrame is then stored in a Google Sheet using the 'generate_progress_report_from_view' function.
+
+    Args:
+        df (DataFrame): The DataFrame containing Masterclass or assessment data in a single column,
+            which needs to be pivoted before reflecting in the Google Sheet.
+        sheet_name (str): The name of the sheet where the pivoted DataFrame should be reflected.
+        value_column (str): The name of the column whose values will be distributed across multiple columns
+            during the DataFrame pivot operation.
+        folder_id (str): The ID of the folder where the Google Sheet is located in Google Drive.
+        file_name (str): The name of the Google Sheet where the pivoted DataFrame should be reflected.
+    """
+
     if not df.empty:
         df.sort_values(value_column, ascending=False, inplace=True)
 
@@ -282,25 +401,35 @@ def generate_report(df, sheet_name, value_column, folder_id, file_name):
         # In case when sheet is related to assessment, we need to fetch one more attribute
         # that is Average
         if sheet_name == "Assessment - Progress":
-            # Except participant_email and participant_name, all others are the assessment, thus
-            # calculate the average for that
+            # TODO: After calculation of average write the logic where the columns that are not there in
+            # view_df but are present in sheet should be added to view_df (Logic is in progress at below)
+            # Sort the records based on the participant_email
+            view_df = update_assessment_data_with_google_sheet_data(
+                new_assessment_data_df=view_df,
+                file_name=file_name,
+                folder_id=folder_id,
+                sheet_name=sheet_name
+            )
 
-            # Before calculating the average, iterate through each assessment and replace the null
-            # values with 0
+            # Before calculating the average, iterate through each assessment and replace the null values with 0
             view_df.fillna(0, inplace=True)
 
             other_columns = ["participant_email", "participant_name"]
-            assessment_columns = [
-                col for col in view_df.columns if col not in other_columns
-            ]
+            assessment_columns = [col for col in view_df.columns if col not in other_columns]
+
+            # Convert the assessment columns into integer
+            for col in assessment_columns:
+                if view_df[col].dtypes != "int64":
+                    view_df[col] = pd.to_numeric(view_df[col], errors='coerce')
+                    view_df[col] = view_df[col].fillna(0).astype("int64")
+
+            # calculate the average
             if len(assessment_columns) > 1:
-                # find the average
+            # Find the average
                 view_df["Average"] = view_df[assessment_columns].mean(axis=1).round()
                 view_df.sort_values("Average", ascending=False, inplace=True)
             else:
-                view_df.sort_values(
-                    assessment_columns[0], ascending=False, inplace=True
-                )
+                view_df.sort_values(assessment_columns[0], ascending=False, inplace=True)
 
         generate_progress_report_from_view(
             view_df=view_df,
@@ -311,3 +440,45 @@ def generate_report(df, sheet_name, value_column, folder_id, file_name):
         print(f"The {sheet_name} report was successfully generated ✅")
     else:
         print(f"There are no {sheet_name} records to update!")
+
+
+def fetch_data_from_google_sheet(file_name, sheet_name, folder_id):
+    """
+    Fetches data from a Google Sheet located in a specific folder.
+
+    Args:
+        file_name (str): The name of the spreadsheet.
+        sheet_name (str): The name of the Google Sheet.
+        folder_id (str): The ID of the folder containing the Google Sheet.
+
+    Returns:
+        pd.DataFrame: A Pandas DataFrame containing the data from the Google Sheet.
+    """
+    # Define the Google Sheets and Drive API scopes
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
+
+    credentials = Credentials.from_service_account_info(
+        service_account_info, scopes=scopes
+    )
+
+    gc = gspread.authorize(credentials)
+
+    try:
+        # First try to open the spreadsheet, if spreadsheet is not there, it will throw the error
+        spreadsheet = gc.open(title=file_name, folder_id=folder_id)
+    except Exception as error:
+        print(error)
+
+    # Select a specific worksheet (e.g., the first sheet)
+    worksheet = spreadsheet.worksheet(sheet_name)
+
+    # Get all values from the worksheet
+    data = worksheet.get_all_values()
+
+    # Convert the data into a DataFrame (assuming the first row contains column headers)
+    df = pd.DataFrame(data[3:], columns=data[2])
+
+    return df
--- main.py
+++ main.py
@@ -9,10 +9,20 @@ from datetime import datetime
 from common import PROGRESS_REPORT_FOLDER_NAME
 
 
-def filter_within_proxy_period(df, months):
+def filter_within_proxy_period(df, months_to_keep):
+    """Filters records in the dataframe to retain only those within the specified proxy period.
+
+    Args:
+        dataframe (DataFrame): The input DataFrame containing records to filter.
+        months_to_keep (int): The number of months that defines the proxy period.
+
+    Returns:
+        DataFrame: A new DataFrame containing only records within the proxy period.
+    """
+
     df["within_proxy_period"] = df["created_at"].apply(
         lambda date_str: date_str.date()
-        > (datetime.now().date() - relativedelta(months=months))
+        > (datetime.now().date() - relativedelta(months=months_to_keep))
     )
 
     df = df[df["within_proxy_period"] == True].reset_index(drop=True)
@@ -24,6 +34,16 @@ def filter_within_proxy_period(df, months):
 
 
 def skills_fact_calculation(cursor, client_id):
+    """Calculates the total scores of a specific project and the individual scores of participants in that project.
+
+    Args:
+        cursor (psycopg2.extensions.cursor): The psycopg2 database cursor for executing SQL queries.
+        client_id (str): The client ID used to filter records related to a specific client and perform calculations.
+
+    Returns:
+        DataFrame: A new DataFrame containing individual learner scores and the total project score, presented as percentages.
+    """
+
     # now fetch the skills_fact table
     cursor.execute(
         f"SELECT * FROM skills_fact WHERE client_id='{client_id}' AND is_current=True;"
@@ -36,7 +56,7 @@ def skills_fact_calculation(cursor, client_id):
     skills_fact_df = pd.DataFrame(skills_fact_result, columns=skills_fact_columns)
 
     # Filter the table based on proxy period
-    skills_fact_df = filter_within_proxy_period(skills_fact_df, months=3)
+    skills_fact_df = filter_within_proxy_period(skills_fact_df, months_to_keep=3)
     # print("Skills fact created_at: ", skills_fact_df["created_at"].unique())
 
     # Take the skills_fact at the order level granularity
@@ -68,6 +88,17 @@ def skills_fact_calculation(cursor, client_id):
 
 
 def progress_fact_calculation(cursor, client_id):
+    """Calculates the progress percentage of participants in their projects. Progress is based on the number of
+    project inputs attempted compared to the total number of inputs.
+
+    Args:
+        cursor (psycopg2.extensions.cursor): The psycopg2 database cursor for executing SQL queries.
+        client_id (str): The client ID used to filter records related to a specific client and perform calculations.
+
+    Returns:
+        DataFrame: A new DataFrame containing the progress percentages of individual learners for their respective projects.
+    """
+
     # now fetch the progress fact table
     cursor.execute(
         f"SELECT * FROM progress_fact WHERE client_id='{client_id}' AND is_current=True;"
@@ -80,13 +111,19 @@ def progress_fact_calculation(cursor, client_id):
     progress_fact_df = pd.DataFrame(progress_fact_result, columns=progress_fact_columns)
 
     # Filter the table based on proxy period
-    progress_fact_df = filter_within_proxy_period(progress_fact_df, months=3)
+    progress_fact_df = filter_within_proxy_period(progress_fact_df, months_to_keep=3)
     # print("Progress fact created_at: ", progress_fact_df["created_at"].unique())
 
     # Now fetch the percentage of progress
     progress_fact_df_calculation = (
         progress_fact_df.groupby(
-            ["participants_name", "participants_email", "order_id", "project_name"],
+            [
+                "participants_name",
+                "participants_email",
+                "order_id",
+                "project_name",
+                "learner_status_by_order",
+            ],
         )
         .agg({"completed_inputs_by_activity": "sum", "total_inputs_by_activity": "sum"})
         .reset_index()
@@ -119,6 +156,18 @@ def progress_fact_calculation(cursor, client_id):
 
 
 def get_folder_id(folder_name, parent_id=None):
+    """Fetches the folder ID of a specified folder in Google Drive. If the folder doesn't exist,
+    it creates the folder and returns its ID.
+
+    Args:
+        folder_name (str): The name of the folder to search for on Google Drive.
+        parent_id (str|None, optional): The ID of the parent folder to search within. If None,
+            the search will be performed in the root directory. Defaults to None.
+
+    Returns:
+        folder_id (str): The folder ID of the specified folder.
+    """
+
     # 1. Check whether Progress Report folder exists or not
     folders = search_folder(folder_name, parent_id)
 
@@ -133,42 +182,48 @@ def get_folder_id(folder_name, parent_id=None):
 
 
 def filter_full_evaluated_assessments(assessment_df):
-    cursor.execute("SELECT * FROM evaluations_status;")
-    result = cursor.fetchall()
-    column_names = [desc[0] for desc in cursor.description]
+    """Filters and fetches assessment data from the 'evaluations_status' table to determine the number of learners
+    who participated in an assessment, how many submitted their projects, and how many were evaluated.
+    The 'is_evaluated' attribute in the evaluation status indicates whether a participant is evaluated or not.
+    Total submitted participants and total evaluated participants are calculated.
+    If the total submitted participants equal the total evaluated participants, their scores are reflected in
+    the Google Sheet. This includes learners who may not have started or submitted their projects but are not counted
+    under total participants.
 
-    evaluations_status_df = pd.DataFrame(result, columns=column_names)
+    Only assessments where the total participants match the evaluated participants are considered in the output.
+    Assessments where these counts differ are excluded.
 
-    evaluations_status_df = evaluations_status_df[
-        ["order_id", "participant_email", "is_evaluated"]
-    ]
+    Args:
+        assessment_df (DataFrame): The DataFrame containing project-wise scores of participants for processing.
 
-    # Now merge the given skills_fact df with the evaluations_df
-    assessment_scores_df = assessment_df.merge(
-        evaluations_status_df,
-        on=["order_id", "participant_email"],
-        how="left",
-    )
-
-    assessment_scores_df.to_csv("assessment_scores.csv", index=False)
+    Returns:
+        fully_evaluated_assessment (DataFrame): The DataFrame containing assessments where the total number of
+        participants matches the number of participants evaluated.
+    """
 
     ## Here we have to do the changes, that although learner haven't attempted the assessment
-    ## his scores should will be visible in the progress_report.
+    ## his scores will be visible in the progress_report.
+    ## In total_participants only those participants are considered who have submitted the project,
+    ## so the script won't wait for the one who haven't submitted the project and once all the participants
+    ## who have submitted the project will be evaluated, the scores of that assessment will be reflected
+    ## in the sheet. And the learner's who haven't submitted the project will contain the 0.
     assessment_evaluations_status_df = (
-        assessment_scores_df.groupby("order_id")
-        # is_evaluated comes in the case of only those participants who have submitted the project,
-        # so if those particiants who haven't submitted or started the project, then he won't be counted
+        assessment_df[assessment_df["learner_status_by_order"] == "Submitted"]
+        .groupby(
+            "project_name"
+        )  # project_name is used insted of order_id, as many times it happend that
+        # same project is deployed separately for some learners due to some issues, but the name of the project
+        # will be the same.
+        # Scores (%) will contain null values in the case when participants haven't evaluated for the project,
+        # thus in total_participants all the participants will count who have submitted the project by counting the
+        # participant_email, while in evaluated particiants only those participants will be consider
+        # who has evaluated the project, the participants who haven't started won't be counted
         # in total_participants, but his scores will be reflected in the progress_report
-        .agg(total_participants=("is_evaluated", "count"))
-        .reset_index()
-        .merge(
-            assessment_scores_df[assessment_scores_df["is_evaluated"] == True]
-            .groupby("order_id")
-            .agg(evaluated_participants=("order_id", "count"))
-            .reset_index(),
-            on="order_id",
-            how="left",
+        .agg(
+            total_participants=("participant_email", "count"),
+            evaluated_participants=("Scores (%)", "count"),
         )
+        .reset_index()
     )
 
     # There can be some assessment where there is no evaluation, in that case there will be blank value
@@ -179,7 +234,7 @@ def filter_full_evaluated_assessments(assessment_df):
 
     # Now merge the assessment_scores_df, with project_evaluations_status_df
     assessment_scores_evaluations_status_df = assessment_evaluations_status_df.merge(
-        assessment_scores_df, on="order_id", how="inner"
+        assessment_df, on="project_name", how="inner"
     )
 
     ## The case when there are no scores for participants indicating that he haven't started the project
@@ -197,7 +252,17 @@ def filter_full_evaluated_assessments(assessment_df):
     return full_evaluated_assessment
 
 
-def generate_report_based_on_program_code(fact_df, folder_id, file_name):
+def separate_and_generate_progress_reports(fact_df, folder_id, file_name):
+    """Separates Masterclass - Progress and Assessment - Progress records based on their intent.
+    Then, it calls the 'generate_report' function from the 'gspread_functions.py' module to generate
+    progress reports for both Masterclass - Progress and Assessment - Progress.
+
+    Args:
+        fact_df (DataFrame): The DataFrame containing both Masterclass and assessment records.
+        folder_id (str): The ID of the folder where the progress report should be located.
+        file_name (str): The name of the file where the progress and assessment reports should be reflected.
+    """
+
     # learning df
     learning_df = fact_df[
         (fact_df["intent"] == "Learning") | (fact_df["intent"] == "Training")
@@ -211,7 +276,7 @@ def generate_report_based_on_program_code(fact_df, folder_id, file_name):
         sheet_name="Masterclass - Progress",
         value_column="Progress (%)",
         folder_id=folder_id,
-        file_name=file_name
+        file_name=file_name,
     )
 
     # assessment df
@@ -247,7 +312,7 @@ def is_program_code(program_code):
     5. If we complete the loop without finding both letters and digits, we return False.
 
     Args:
-        program_code (_type_): _description_
+        program_code (str):
     """
     has_letters = False
     has_digits = False
@@ -266,6 +331,18 @@ def is_program_code(program_code):
 
 
 def main():
+    """The main function where containing the main logic where the clients records, orders_dimensions
+    records are calculated and then get_folder_id function is called to fetch the folder id if exists
+    or create if not exists
+
+    The client_id and client_name are mapped and then one by one all the scores of the programs will be
+    filtered out based on the client_id that are currently running.
+
+    The description of the projects are fetched from the 'orders_dimension' table, using the description
+    program codes are fetched and check whether the program code is appropriate or not if yes then it
+    continues and filter all the records based on that program_code.
+    """
+
     # Fetch the folder_id of progress_report
     # This will search for folder, if folder is not there it will create the folder and return the id
     # else will directly return the id
@@ -380,6 +457,7 @@ def main():
                     "Progress (%)",
                     "project_name",
                     "intent",
+                    "learner_status_by_order",
                 ]
             ]
 
@@ -387,7 +465,7 @@ def main():
             file_name = f"{program_code}_report"
 
             # it will take program code and based on that, it will filter the
-            generate_report_based_on_program_code(
+            separate_and_generate_progress_reports(
                 fact_df=view_df,
                 folder_id=program_code_folder_id,  # the folder where the reports sheet will be created that is Program code folder
                 file_name=file_name,
